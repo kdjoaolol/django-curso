c4b48bb468abe4afd966cdf538448328e1deb57f
kdjoaolol <kdjoaolol123@gmail.com>
2025-12-18 19:52:08 -0300

    verificando como funciona as views com render!

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def my_view(request):
-    return HttpResponse('RESPOSTA')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view)
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')), #dominio.com/recipes/
 ]
